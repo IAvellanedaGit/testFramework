8476e40fbe4d9aae159e7547d2dae6fcf92b6801
Iban Avellaneda <iban.avellaneda@abbott.com>
2023-05-01 23:35:45 -0500

    test framework init

---
+++ TestFramework/TestFramework/TestStuffHere.swift
@@ -0,0 +1,14 @@
+//
+//  TestStuffHere.swift
+//  TestFramework
+//
+//  Created by Iban Avellaneda on 5/1/23.
+//
+
+import Foundation
+
+public class TestingClass {
+    public func printSomeCoolStuff() {
+        print("Some Cool Stuff")
+    }
+}
